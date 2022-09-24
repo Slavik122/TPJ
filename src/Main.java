915ba5e03be1808aabadc4b0006c4da04a972fb5
Slavik Yelkin <slavikelkin23445@gmail.com>
2022-09-24 22:31:55 +0300

    Add new sout element

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("This task done by Yelkin Viacheslav SE-19-1/9");
+        System.out.println("This is new message line");
     }
 }
