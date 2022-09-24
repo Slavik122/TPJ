e423b20426040a277f6576bc5487922d09fbacf8
Slavik122 <57828548+Slavik122@users.noreply.github.com>
2022-09-24 23:03:47 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This task done by Yelkin Viacheslav SE-19-1/9");
         System.out.println("This is new message line");
-        System.out.println("Line added in github file editor")
+        System.out.println("Line added in github file editor");
     }
 }
