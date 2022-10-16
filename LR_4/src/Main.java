1e84b91aed8b0946abb55e40824291b3aed3230e
Slavik Yelkin <slavikelkin23445@gmail.com>
2022-10-16 14:17:54 +0300

    init commit

---
+++ LR_4/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*Print emoji*/
+        System.out.printf("%s %s %s \n","\ud83d\ude0a","\ud83e\udd13","\ud83e\udd14");
+
+
+        String str = new String("11.9");
+        String str2 = new String("8.3");
+
+        /*Print sum of two string type, double numbers*/
+        System.out.printf("%.2f ", (Double.parseDouble(str) + Double.parseDouble(str2)));
+    }
+}
